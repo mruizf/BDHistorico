26e4671597c4689fee479780cc25ca35b5703c26
M R <mruiz@gmail.com>
2025-08-23 15:53:12 -0600

    - Agregamos los .csv a .gitignore - Tenemos 2 Mapas de valores: las mapas de valores raw (excel + ids+ otros) y el mapa de valores para la BD, que se pasa a las queries que usan parametros named(:idfibra, :valor, :fecha) - Agregamos funciones de apovo para contruir el mapa completo con los datos raw y no hacerlo en medio del código - Limpiamos el código poniendo en mayusculas las variables que son constantes - Para no tener cadenas sueltas en medio del codigo, hicimos dos clases que tienen los nombres de los campos de la hoja de excel y de la base de datos

--- CreacionDB.py
+++ CreacionDB.py
@@ -13,7 +13,7 @@ import queries as qu
 
 
 ########################VARIABLES GLOBALES#############################
-fibras : List[str] = ["DANHOS13",
+FIBRAS : List[str] = ["DANHOS13",
                     "FUNO11",
                     "FMTY14",
                     "FNOVA17",
@@ -28,23 +28,26 @@ fibras : List[str] = ["DANHOS13",
                     "FHIPO14"]
 
 
-dbName:str       ="historicoFibras.db"
-schemaFile:str   ="schema.sql"
+DBNAME:str       ="historicoFibras.db"
+SCHEMAFILE:str   ="schema.sql"
 
-create_database = True
-insertar_fibras:bool = True
-insertar_distribuciones:bool =True
-insertar_velas:bool =True
+EXCEL_FILE_DIVIDENDOS:str ="Historico de Fibras.xlsx"
+EXCEL_FILE_VELAS_DIARIAS:str ="HistoricoVelasDesde2024.xlsx"
+
+CREATE_DATABASE = True
+INSERTAR_FIBRAS:bool = True
+INSERTAR_DISTRIBUCIONES:bool =True
+INSERTAR_VELAS:bool =True
 
 
 
 def create_db() -> NoReturn:
-    if not os.path.exists(dbName):
+    if not os.path.exists(DBNAME):
         print("Base de datos no encontrada. Creando nueva...")
-        conn = sqlite3.connect(dbName)
+        conn = sqlite3.connect(DBNAME)
         cursor = conn.cursor()
 
-        with open(schemaFile, "r", encoding="utf-8") as f:
+        with open(SCHEMAFILE, "r", encoding="utf-8") as f:
             schema = f.read()
             cursor.executescript(schema)
 
@@ -59,96 +62,63 @@ def create_db() -> NoReturn:
 
 if __name__=="__main__":
 
-    if create_database:
+    if CREATE_DATABASE:
         create_db()
         
-    if insertar_fibras:
-        for fibra in fibras:
+    if INSERTAR_FIBRAS:
+        for fibra in FIBRAS:
             print(f"Fibra:{fibra}")
             #aqui vamos a insertar las velas
-            with sqlite3.connect(dbName) as conn:
+            with sqlite3.connect(DBNAME) as conn:
                 insert = qu.InsertNewFibra(fibra)
                 insert.execute(conn)
     
-    if insertar_velas:
-        hojas:dict[str,pd.DataFrame] = pd.read_excel("HistoricoVelasDesde2024.xlsx", sheet_name=None)        
+    if INSERTAR_VELAS:
+        hojas:dict[str,pd.DataFrame] = pd.read_excel(EXCEL_FILE_VELAS_DIARIAS, sheet_name=None)        
     
-        for fibra in fibras:
+        for fibra in FIBRAS:
             print(f"Fibra:{fibra}")
             
             if fibra in hojas:
                 dft = hojas[fibra]
-                with sqlite3.connect(dbName) as conn:
+                with sqlite3.connect(DBNAME) as conn:
                     print("Guardando la hoja"+fibra)
     
                     select = qu.GetFibraID(fibra)
-                    resultado = select.execute(conn)
+                    id_fibra = select.execute(conn)
                     
-                    if resultado:  
-                        id_fibra = resultado[0][0]   # fetalldone devuelve una lista de tuplas
-                        print("El id_fibra es:", id_fibra)
-                    else:
-                        print("No se encontró la fibra")
                         
                     print("Ahora insertamos las velas")
-                                
-                    columnNames= list(dft.columns.values)
                     for index,row in dft.iterrows():
-                        temp = row["Vol."]
-                        volume=-1
-                        if (temp[-1]=="K"):
-                            volume = float(temp[0:-1])*1000
-                        elif (temp[-1]=="M"):
-                            volume = float(temp[0:-1])*1000000
-                        else:
-                            print("Posfijo inesperado: "+temp)
-                        
-                        #volume:float  = temp
-                        #var:float = row["% var."]
-                                            
-                        
-                        #insertamos en la BD
-                        valores = row
-                        valores["Fecha"]=int(row["Fecha"].timestamp())
-                        valores["Vol."]=volume
-                        valores["id_fibra"]=id_fibra
-                        valores["frecuencia"]="1D"
-                        
-                        insertVela = qu.InsertVela(valores)
+                        valoresRaw = qu.BuildOriginalDataRow(id_fibra,row)
+                        insertVela = qu.InsertVela(valoresRaw)
                         insertVela.execute(conn)
                     conn.commit()
                 print(f"Fibra '{fibra}' insertada con éxito.")
 
 
 
-    if insertar_distribuciones:
-        hojas:dict[str,pd.DataFrame] = pd.read_excel("Historico de Fibras.xlsx", header=1,sheet_name=None)
+    if INSERTAR_DISTRIBUCIONES:
+        hojas:dict[str,pd.DataFrame] = pd.read_excel(EXCEL_FILE_DIVIDENDOS, header=1,sheet_name=None)
         
-        for fibra in fibras:
+        for fibra in FIBRAS:
             print(f"Fibra:{fibra}")
             if fibra in hojas:
                 dft = hojas[fibra]
-                with sqlite3.connect(dbName) as conn:
+                with sqlite3.connect(DBNAME) as conn:
                     print("Guardando la hoja distribucion"+fibra)
                                         
                     select = qu.GetFibraID(fibra)
-                    resultado = select.execute(conn)
-        
-                    id_fibra=-1
-                    if resultado:  
-                        id_fibra = resultado[0][0]   # fetalldone devuelve una lista de tuplas
-                        print("El id_fibra es:", id_fibra)
-                    else:
-                        print("No se encontró la fibra")
+                    id_fibra= select.execute(conn)
                     
                     print("Ahora insertamos las distribuciones")
                     
                     for index,row in dft.iterrows():
                         #variables temporales
                         valores = row
-                        valores["Fecha de pago"]=int(row["Fecha de pago"].timestamp())
-                        valores["Fecha ex-dividendo"]=int(row["Fecha ex-dividendo"].timestamp())
-                        valores["id_fibra"]=id_fibra
+                        valores[qu.EXC_DIV.FECHA_PAGO]=int(row[qu.EXC_DIV.FECHA_PAGO].timestamp())
+                        valores[qu.EXC_DIV.FECHA_EX_DIV]=int(row[qu.EXC_DIV.FECHA_EX_DIV].timestamp())
+                        valores[qu.EXC_DIV.ID_FIBRA]=id_fibra
                         
                         #insertamos en la BD
                         insertDividendo = qu.InsertDividendo(valores)
--- queries.py
+++ queries.py
@@ -5,15 +5,100 @@ Created on Tue Aug 19 13:12:44 2025
 @author: M R
 """
 import sqlite3
-from abc import ABC, abstractmethod
 
 #Query Objects
 #Usados para leer datos de una hoja de excel e insertarlos en la base sqlite
 #Para mantener esto sencillo tendremos una hoja de calculo por cada tabla
 
-class QueryBase(ABC):
-    SQL=""
+
+
+class DB_DIV:
+    ID_FIBRA    ="id_fibra"
+    FECHA_EX_DIV="fecha_ex_dividendo"
+    FECHA_PAGO  ="fecha_pago"
+    TIPO        ="tipo_dividendo"
+    DIVIDENDO   ="dividendo"
+    RENDIMIENTO ="rendimiento"
+class EXC_DIV:
+    ID_FIBRA    ="id_fibra"
+    FECHA_EX_DIV="Fecha ex-dividendo"
+    FECHA_PAGO  ="Fecha de pago"
+    TIPO        ="Tipo"
+    DIVIDENDO   ="Dividendo"
+    RENDIMIENTO ="Rendimiento"
+
+DB_DIVIDENDOS_2_EXCEL={
+    DB_DIV.ID_FIBRA    : EXC_DIV.ID_FIBRA,
+    DB_DIV.FECHA_EX_DIV: EXC_DIV.FECHA_EX_DIV,
+    DB_DIV.FECHA_PAGO  : EXC_DIV.FECHA_PAGO,
+    DB_DIV.TIPO        : EXC_DIV.TIPO,
+    DB_DIV.DIVIDENDO   : EXC_DIV.DIVIDENDO,
+    DB_DIV.RENDIMIENTO : EXC_DIV.RENDIMIENTO
+    }
+
+class DB_VELAS:
+    ID_FIBRA  ="id_fibra"
+    TIMESTAMP ="timestamp"
+    OPEN      ="open"
+    HIGH      ="high"
+    LOW       ="low"
+    CLOSE     ="close"
+    VOLUME    ="volume"
+    VAR       ="var"
+    FRECUENCIA="frecuencia"
+
+class EXC_VELAS:
+    ID_FIBRA  ="id_fibra"
+    TIMESTAMP ="Fecha"
+    OPEN      ="Apertura"
+    HIGH      ="Máximo"
+    LOW       ="Mínimo"
+    CLOSE     ="Cierre"
+    VOLUME    ="Vol."
+    VAR       ="% var."
+    FRECUENCIA="frecuencia"
     
+DB_VELAS_2_EXCEL={
+    DB_VELAS.ID_FIBRA  :EXC_VELAS.ID_FIBRA  ,
+    DB_VELAS.TIMESTAMP :EXC_VELAS.TIMESTAMP ,
+    DB_VELAS.OPEN      :EXC_VELAS.OPEN      ,
+    DB_VELAS.HIGH      :EXC_VELAS.HIGH      ,
+    DB_VELAS.LOW       :EXC_VELAS.LOW       ,
+    DB_VELAS.CLOSE     :EXC_VELAS.CLOSE     ,
+    DB_VELAS.VOLUME    :EXC_VELAS.VOLUME    ,
+    DB_VELAS.VAR       :EXC_VELAS.VAR       ,
+    DB_VELAS.FRECUENCIA:EXC_VELAS.FRECUENCIA}
+
+
+def getColsAndPlaceholders(MapaLlaves)-> tuple[str ,str] :
+    cols = ",".join(MapaLlaves.keys())
+    placeholders = ",".join([f":{k}" for k in MapaLlaves.keys()])
+    return cols,placeholders
+
+def BuildOriginalDataRow(id_fibra,row):
+    temp = row[EXC_VELAS.VOLUME]
+    volume=-1
+    if (temp[-1]=="K"):
+        volume = float(temp[0:-1])*1000
+    elif (temp[-1]=="M"):
+        volume = float(temp[0:-1])*1000000
+    else:
+        print("Posfijo inesperado: "+temp)
+    
+    
+    valores = row
+    valores[EXC_VELAS.TIMESTAMP]=int(row[EXC_VELAS.TIMESTAMP].timestamp())
+    valores[EXC_VELAS.VOLUME]=volume
+    valores[EXC_VELAS.ID_FIBRA]=id_fibra
+    valores[EXC_VELAS.FRECUENCIA]="1D"
+    
+    return valores
+
+
+class QueryBase():
+    SQL=""
+    #mapa de objetos, cuyas llaves estan en el query en formato named parameters 
+    #(parámetros con nombre)
     registro={}
 
     def execute(self,conn):
@@ -38,39 +123,31 @@ class GetFibraID(QueryBase):
     def __init__(self, id_fibra=-1):
         self.registro={"id_fibra":id_fibra}
         self.SQL = "SELECT id_fibra FROM fibras WHERE nombre = :id_fibra"
+        
+    def execute(self,conn):
+        """Aqui llamamos a la ejecución de la query y pelamos el resultado"""
+        resultados = super().execute(conn)
+        
+        if resultados:  
+            id_fibra = resultados[0][0]   # fetalldone devuelve una lista de tuplas
+            return id_fibra
+        else:
+            print("No se encontró la fibra")
+            return None
+
 
 class InsertDividendo(QueryBase):
-    DB2EXCEL={"id_fibra":"id_fibra",
-              "fecha_ex_dividendo":"Fecha ex-dividendo",
-              "fecha_pago": "Fecha de pago",
-              "tipo_dividendo": "Tipo",
-              "dividendo": "Dividendo",
-              "rendimiento": "Rendimiento"}
-    
-    def __init__(self,valores):
-        cols = ",".join(self.DB2EXCEL.keys())
-        placeholders = ",".join([f":{k}" for k in self.DB2EXCEL.keys()])
-        
+    def __init__(self,valores):      
+        cols, placeholders = getColsAndPlaceholders(DB_DIVIDENDOS_2_EXCEL)
         self.SQL = f"INSERT INTO distribuciones ({cols}) VALUES ({placeholders})"
-        for k in self.DB2EXCEL.keys():
-             self.registro[k]=valores[self.DB2EXCEL[k]]
+        
+        for k in DB_DIVIDENDOS_2_EXCEL.keys():
+             self.registro[k]=valores[DB_DIVIDENDOS_2_EXCEL[k]]
     
 class InsertVela(QueryBase):
-    DB2EXCEL={ "id_fibra":"id_fibra",
-               "timestamp":"Fecha",
-               "open":"Apertura",
-               "high":"Máximo",
-               "low":"Mínimo",
-               "close":"Cierre",
-               "volume":"Vol.",
-               "var":"% var.",
-               "frecuencia":"frecuencia"}
-    
     def __init__(self, valores):
-        cols = ",".join(self.DB2EXCEL.keys())
-        placeholders = ",".join([f":{k}" for k in self.DB2EXCEL.keys()])
-      
+        cols, placeholders = getColsAndPlaceholders(DB_VELAS_2_EXCEL)
         self.SQL = f"INSERT INTO velas ({cols}) VALUES ({placeholders})"
         
-        for k in self.DB2EXCEL.keys():
-             self.registro[k]=valores[self.DB2EXCEL[k]]
+        for k in DB_VELAS_2_EXCEL.keys():
+             self.registro[k]=valores[DB_VELAS_2_EXCEL[k]]
